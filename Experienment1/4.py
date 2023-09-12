832e27881dbeb9d9e075508129f05ec39c977ce3
muiyu <762893143@qq.com>
2023-09-12 18:56:48 +0800

    Solve Q6 and add 4.py

---
+++ Experienment1/4.py
@@ -0,0 +1,9 @@
+A = input()
+list = A.split()
+try:
+    values = [int(value) for value in list]
+except ValueError:
+    print("输入无效。请确保所有输入都是有效的整数。")
+sorted_values = sorted(values, reverse=True)
+
+print(sorted_values[0], sorted_values[1], sorted_values[2], sorted_values[3])
